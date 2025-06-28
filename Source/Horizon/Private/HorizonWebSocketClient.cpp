19b8c0e0b0a179da1a47f62aa324781f849dc2b7
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2025-06-28 12:25:55 -0400

    Updates

--- Source/Horizon/Private/HorizonWebSocketClient.cpp
+++ Source/Horizon/Private/HorizonWebSocketClient.cpp
@@ -1,5 +1,3 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 #include "HorizonWebSocketClient.h"
 #include "Horizon.h"
 #include "Engine/World.h"
--- Source/Horizon/Private/HorizonWebsocketComponent.cpp
+++ Source/Horizon/Private/HorizonWebsocketComponent.cpp
@@ -1,5 +1,3 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 #include "HorizonWebSocketComponent.h"
 #include "HorizonWebSocketClient.h"
 #include "Horizon.h"
